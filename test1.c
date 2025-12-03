2de1eba0068fe3a17230dc52d74a4ffecca1f0ea
jingu1234 <jinguwow1@gmail.com>
2025-12-03 19:05:12 +0900

    sum to mul

---
+++ test1.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+
+int main() {
+    int a, b, mul;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    mul = a * b;
+    printf("Multiple: %d\n", mul);
+    return 0;
+
+rm
+}
